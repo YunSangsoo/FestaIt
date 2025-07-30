1cb09487b490127a54a03cc87fabffbe4ce3c7ea
kdhiop <kdhiop@gmail.com>
2025-07-30 12:17:32 +0900

    리뷰 작업

---
+++ src/main/java/com/kh/festait/reviewboard/model/dao/ReviewBoardDao.java
@@ -0,0 +1,23 @@
+package com.kh.festait.reviewboard.model.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.kh.festait.reviewboard.model.vo.ReviewBoard;
+
+@Mapper
+public interface ReviewBoardDao {
+
+	List<ReviewBoard> selectReviewListWithPaging(Map<String, Object> paramMap);
+
+	int getReviewCount();
+
+	int insertReview(ReviewBoard review);
+
+	int updateByUserNo(ReviewBoard review);
+
+	int deleteByUserNo(int userNo);
+
+}
---
+++ src/main/java/com/kh/festait/reviewboard/model/service/ReviewBoardService.java
@@ -0,0 +1,21 @@
+package com.kh.festait.reviewboard.model.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.kh.festait.noticeboard.model.vo.NoticeBoard;
+import com.kh.festait.reviewboard.model.vo.ReviewBoard;
+
+public interface ReviewBoardService {
+
+	List<ReviewBoard> selectReviewList(int offset, int limit);
+
+	int getReviewCount();
+
+	int insertReview(ReviewBoard review);
+
+	int updateReviewByUserNo(ReviewBoard review);
+
+	int deleteReviewByUserNo(int userNo);
+
+}
